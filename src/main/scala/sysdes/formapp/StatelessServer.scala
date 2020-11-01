fb8d4857ee46a16b1c090e790ff86d036619a645
Takuma515 <red-puma2410@docomo.ne.jp>
2020-11-01 17:03:45 +0900

    ステートレスサーバの完了

--- src/main/scala/sysdes/formapp/StatelessServer.scala
+++ src/main/scala/sysdes/formapp/StatelessServer.scala
@@ -1,9 +1,10 @@
 package sysdes.formapp
 
-import java.net.Socket
+import java.net.{Socket, URLDecoder}
+
 import sysdes.formapp.server.{Handler, Server}
 
-object StatelessServer extends Server(8001) {
+object StatelessServer extends Server(8000) {  //ポート番号を変更
   override def getHandler(socket: Socket) = new StatelessServerHandler(socket)
 }
 
@@ -11,19 +12,132 @@ class StatelessServerHandler(socket: Socket) extends Handler(socket) {
 
   import sysdes.formapp.server.{NotFound, Ok, Request, Response}
 
+
+
   override def handle(request: Request): Response = request match {
     case Request("GET", "/", _, _, _) => index()
-    case _                            => NotFound(s"Requested resource '${request.path}' for ${request.method} is not found.")
+    case Request("POST", "/name", _, _, _) => name()
+    case Request("POST", "/gender", _, _, Some(body)) => gender(body)
+    case Request("POST", "/message", _, _, Some(body)) => message(body)
+    case Request("POST", "/confirm", _, _, Some(body)) => confirm(body)
+    case Request("POST", "/complete", _, _, _) => complete()
+    case _ => NotFound(s"Requested resource '${request.path}' for ${request.method} is not found.")
   }
 
+  /* 1.スタート画面 */
   def index(): Response = {
-    Ok("""<html>
+    Ok("""<html lang="ja">
+        |<head>
+        |    <meta charset="UTF-8">
+        |</head>
+        |
         |<body>
-        |    <form action="/form" method="post">
+        |    アンケート開始<br>
+        |    <form action="/name" method="post">
         |        <input type="submit" value="start" />
         |    </form>
         |</body>
         |</html>""".stripMargin)
   }
 
+
+  /* 2.名前入力画面 */
+  def name(): Response = {
+    Ok(
+      """<html lang="ja">
+        |<head>
+        |    <meta charset="UTF-8">
+        |</head>
+        |
+        |<body>
+        |    <form action="/gender" method="post">
+        |    名前：<input type="text" name="name"><br>
+        |    <input type="submit" value="next" />
+        |    </form>
+        |</body>
+        |</html>""".stripMargin)
+  }
+
+  /* 3.性別入力画面 */
+  def gender(body: String): Response = {
+    System.out.println("gender")
+    System.out.println(body)
+    val parameters: Array[String] = body.split("=")
+
+    Ok(
+      s"""<html lang="ja">
+        |<head>
+        |    <meta charset="UTF-8">
+        |</head>
+        |<body>
+        |    <form action="/message" method="post">
+        |    <input type="hidden" name="name" value="${URLDecoder.decode(parameters(1),"UTF-8")}">
+        |    性別：
+        |    <input type="radio" name="gender" value="male"> 男性
+        |    <input type="radio" name="gender" value="female"> 女性<br>
+        |    <input type="submit" value="next"/>
+        |    </form>
+        |</body>
+        |</html>""".stripMargin)
+  }
+
+  /* 4.メッセージ入力画面 */
+  def message(body: String): Response = {
+    System.out.println("message")
+    System.out.println(body)
+    val parameters: Array[String] = body.split("[=&]")
+
+    Ok(
+      s"""<html lang="ja">
+        |<head>
+        |    <meta charset="UTF-8">
+        |</head>
+        |
+        |<body>
+        |    <form action="/confirm" method="post">
+        |    <input type="hidden" name="name" value="${URLDecoder.decode(parameters(1),"UTF-8")}">
+        |    <input type="hidden" name="gender" value="${parameters(3)}">
+        |    メッセージ：<br>
+        |    <textarea name="message" placeholder="メッセージを入力"></textarea><br>
+        |    <input type="submit" value="next" />
+        |    </form>
+        |</body>
+        |</html>""".stripMargin)
+  }
+
+  /* 5.確認画面 */
+  def confirm(body: String): Response = {
+    val parameters: Array[String] = body.split("[=&]")
+
+    Ok(
+      s"""<html lang="ja">
+        |<head>
+        |    <meta charset="UTF-8">
+        |</head>
+        |
+        |<body>
+        |    名前：${URLDecoder.decode(parameters(1),"UTF-8")}<br>
+        |    性別：${URLDecoder.decode(parameters(3),"UTF-8")}<br>
+        |    メッセージ：${URLDecoder.decode(parameters(5),"UTF-8")}<br>
+        |    <form action="/complete" method="post">
+        |        <input type="submit" value="submit"/>
+        |    </form>
+        |</body>
+        |</html>""".stripMargin)
+  }
+
+  /*6.送信完了画面 */
+  def complete(): Response = {
+    Ok(
+      s"""<html lang="ja">
+         |<head>
+         |    <meta charset="UTF-8">
+         |</head>
+         |
+         |<body>
+         |    送信が完了しました<br>
+         |    <a href="/">スタート画面へ戻る</a>
+         |</body>
+         |</html>""".stripMargin)
+  }
 }
